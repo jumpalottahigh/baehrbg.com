93c61b2d3b67315e2733bb6e1436754c62f451db
Georgi Yanev <georgiyanev.gy@gmail.com>
2018-09-22 13:57:44 +0300

    Fix 404 page layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 import Layout from '../components/Layout/Layout'
+
+const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 10px;
+  text-align: center;
+  padding-top: 80px;
+  min-height: 35vh !important;
+`
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Container>
   </Layout>
 )
 
