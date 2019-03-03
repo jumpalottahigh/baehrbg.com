1d8b949f96b6904cb8eef2e257a22553b8a653a2
Georgi Yanev <georgiyanev.gy@gmail.com>
2019-03-03 22:07:08 +0200

    Swap carousel angle brackets for prettier ones

---
+++ src/components/Contacts/Button.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+  color: #fff;
+  border: 0;
+  margin: 0;
+  cursor: pointer;
+  padding: 8px 16px;
+  font-size: 0.875rem;
+  min-width: 64px;
+  min-height: 36px;
+  font-weight: 500;
+  line-height: 1.5;
+  border-radius: 4px;
+  letter-spacing: 0.02857em;
+  background: #c5112e;
+  box-shadow: 0px 1px 5px 0px rgba(0, 0, 0, 0.2),
+    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12);
+`
+
+export default Button
